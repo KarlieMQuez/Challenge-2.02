de3a3e74aa200aaa4fd907ff2d721259c65738eb
KarlieMQuez <157342147+KarlieMQuez@users.noreply.github.com>
2024-02-19 22:21:55 -0800

    update

--- js/intro.js
+++ js/intro.js
@@ -1,2 +1,37 @@
 //resource link: https://www.w3schools.com/jquery/default.asp
 
+$(document).ready(function() {
+
+$("#secret-box").hide();
+
+$("#box-button").click( function() {
+    $("#secret-box").toggle();
+
+});
+
+$("#secret-box").click( function(){
+    $("#secret-box").css("background-color","palegreen")
+    $("#secret-box").addClass("animate-boxy");
+    
+    setTimeout(function (){
+        $("#secret-box").removeClass
+        ("animate-boxy");
+    },5000);
+
+});
+
+$("#my-name").click( function(){
+    $("#secret-box").css("border-radius","20%");
+    $("#secret-box").css("background-color","purple");
+});
+
+$("#redo").click(function(){
+    $("#secret-box").hide();
+    $("#secret-box").css("background-color","rosybrown");
+    $("secret-box").css("border-radius","0px");
+
+})
+
+
+
+});
